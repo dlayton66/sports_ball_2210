25d4e6d8a9eb1d98a7a6643c15588f62fe1da39b
Drew Layton <dlayton66@gmail.com>
2022-10-20 13:50:47 -0600

    feat: Add attributes to Team class per Iteration 3

--- lib/team.rb
+++ lib/team.rb
@@ -1,16 +1,34 @@
 class Team
-  attr_reader :mascot, :city, :roster
+  attr_reader :mascot, :city, :roster, :long_term_players, :short_term_players,
+              :details
   def initialize(mascot,city)
     @mascot = mascot
     @city = city
     @roster = []
+    @long_term_players = []
+    @short_term_players = []
+    @details = {
+      "total_value" => 0,
+      "player_count" => 0
+    }
   end
 
   def player_count
-    roster.count
+    details["player_count"]
+  end
+
+  def total_value
+    details["total_value"]
   end
 
   def add_player(player)
     roster << player
+    details["player_count"] += 1
+      if player.contract_length > 24
+        long_term_players << player
+      else
+        short_term_players << player
+      end
+    details["total_value"] += player.total_cost
   end
 end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -42,7 +42,7 @@ RSpec.describe Team do
     end
   end
 
-  describe '#long_term_players' do
+  describe 'long_term_players' do
     it 'lists players with contracts > 2 years' do
       team = Team.new("Dodgers", "Los Angeles")
       player_1 = Player.new("Michael Palledorous" , 1000000, 36)
@@ -58,7 +58,7 @@ RSpec.describe Team do
     end
   end
 
-  describe '#short_term_players' do
+  describe 'short_term_players' do
     it 'lists players with contracts <= 2 years' do
       team = Team.new("Dodgers", "Los Angeles")
       player_1 = Player.new("Michael Palledorous" , 1000000, 36)
@@ -74,8 +74,8 @@ RSpec.describe Team do
     end
   end
 
-  describe '#total_cost' do
-    it 'returns the total cost of all players' do
+  describe 'total_value' do
+    it 'returns the total value of all players' do
       team = Team.new("Dodgers", "Los Angeles")
       player_1 = Player.new("Michael Palledorous" , 1000000, 36)
       player_2 = Player.new("Kenny DeNunez", 500000, 24)
@@ -90,8 +90,8 @@ RSpec.describe Team do
     end
   end
 
-  describe '#details' do
-    it 'returns a hash with total_cost and player_count' do
+  describe 'details' do
+    it 'returns a hash with total_value and player_count' do
       team = Team.new("Dodgers", "Los Angeles")
       player_1 = Player.new("Michael Palledorous" , 1000000, 36)
       player_2 = Player.new("Kenny DeNunez", 500000, 24)
@@ -102,7 +102,7 @@ RSpec.describe Team do
       team.add_player(player_3)
       team.add_player(player_4)
 
-      expect(team.details).to eq {"total_value" => 85200000, "player_count" => 4}
+      expect(team.details).to eq({"total_value" => 85200000, "player_count" => 4})
     end
   end
 end

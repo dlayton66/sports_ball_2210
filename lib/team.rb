ccbf85afbfb2e6294fec1ce0376563b5b13527c4
Drew Layton <dlayton66@gmail.com>
2022-10-20 17:57:07 -0600

    feat: Finish Iteration 4

--- lib/team.rb
+++ lib/team.rb
@@ -1,12 +1,13 @@
 class Team
   attr_reader :mascot, :city, :roster, :long_term_players, :short_term_players,
-              :details
+              :lastnames, :details
   def initialize(mascot,city)
     @mascot = mascot
     @city = city
     @roster = []
     @long_term_players = []
     @short_term_players = []
+    @lastnames = []
     @details = {
       "total_value" => 0,
       "player_count" => 0
@@ -23,6 +24,7 @@ class Team
 
   def add_player(player)
     roster << player
+    lastnames << player.last_name
     details["player_count"] += 1
       if player.contract_length > 24
         long_term_players << player
@@ -30,5 +32,19 @@ class Team
         short_term_players << player
       end
     details["total_value"] += player.total_cost
+  end
+
+  def average_cost_of_player
+    cost = (details["total_value"]/details["player_count"]).to_s
+    n = cost.length
+    while n > 3 do
+      n-=3
+      cost.insert(n,",")
+    end
+    cost.insert(0,"$")
+  end
+
+  def players_by_last_name
+    lastnames.sort.join(", ")
   end
 end
